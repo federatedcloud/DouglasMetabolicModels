96afe90cdbe7203d56fb17333a4abdabb219c341
Brandon Elam Barker <beb82@cornell.edu>
2019-02-22 13:39:11 +0000

    ecoli_basicTest running again with new model reorg

--- Tests/ecoli_basicTest.m
+++ Tests/ecoli_basicTest.m
@@ -1,4 +1,6 @@
-iAF1260 = load('iAF1260.mat')
+function [sol, res] = ecoli_basicTest
+
+iAF1260 = load('../models/iAF1260.mat')
 
 ecoli=iAF1260.iAF1260
 %model_1 = readCbModel('ecoli_1.2_cobra.xml')
@@ -89,42 +91,40 @@ options = struct();
 options.GRguess = 0.5;  % initial guess for max. growth rate
 options.GRtol = 1e-6;  % tolerance for final growth rate
 options.algorithm = 1;  % use the default algorithm (simple guessing for bounds, followed by matlab fzero)
-[sol, result] = SteadyCom(EcCom, options);
+[sol, res] = SteadyCom(EcCom, options);
 
 
+% FIXME, Note from Brandon Barker: the below code is not working and
+% almost seems to be tacked on; haven't had a chance
+% to ascertain its provenance as yet.
 
 
+% %%
+% growth = "growth";
+
+% if model_1.rxns{find(model_1.c)} == ""
+%     model_1.rxns{find(model_1.c)} = 'growth';
+% else
+%     growth = model_1.rxns(find(model_1.c));
+% end
+
+% model_2 = model_1;
+% model_3 = model_1;
+
+% % to avoid errors associated with PubChemID column in the createMultipleSpeciesModel
+% %model_2.metPubChemID = num2cell(model_2.metPubChemID)
+% %model_3.metPubChemID = num2cell(model_3.metPubChemID)
 
 
+% multi_model =  createMultipleSpeciesModel({model_1; model_3}, {'Org1'; 'Org2'});
+% [multi_model.infoCom, multi_model.indCom] = getMultiSpeciesModelId(multi_model, {'Org1'; 'Org2'});
 
+% multi_model.csense = char('E' * ones(1,numel(multi_model.mets)));  % correct the csense
 
+% disp( multi_model.infoCom)
+% disp( multi_model.indCom)
 
-%%
-growth = "growth";
-
-if model_1.rxns{find(model_1.c)} == ""
-    model_1.rxns{find(model_1.c)} = 'growth';
-else 
-    growth = model_1.rxns(find(model_1.c));
-end
-
-model_2 = model_1;
-model_3 = model_1;
-
-% to avoid errors associated with PubChemID column in the createMultipleSpeciesModel 
-%model_2.metPubChemID = num2cell(model_2.metPubChemID)
-%model_3.metPubChemID = num2cell(model_3.metPubChemID)
- 
-
-multi_model =  createMultipleSpeciesModel({model_1; model_3}, {'Org1'; 'Org2'});
-[multi_model.infoCom, multi_model.indCom] = getMultiSpeciesModelId(multi_model, {'Org1'; 'Org2'});
-
-multi_model.csense = char('E' * ones(1,numel(multi_model.mets)));  % correct the csense
-
-disp( multi_model.infoCom)
-disp( multi_model.indCom)
-
-rxnBiomass = strcat({'Org1'; 'Org2'}, growth);  % biomass reaction names
-rxnBiomassId = findRxnIDs(multi_model, rxnBiomass);  % ids
-multi_model.infoCom.spBm = rxnBiomass;  % .spBm for organism biomass reactions
-multi_model.indCom.spBm = rxnBiomassId;
+% rxnBiomass = strcat({'Org1'; 'Org2'}, growth);  % biomass reaction names
+% rxnBiomassId = findRxnIDs(multi_model, rxnBiomass);  % ids
+% multi_model.infoCom.spBm = rxnBiomass;  % .spBm for organism biomass reactions
+% multi_model.indCom.spBm = rxnBiomassId;
