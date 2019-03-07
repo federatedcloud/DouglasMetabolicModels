3588f70dbb9a9d1d999d9a39b3b19157c2e866d0
Brandon Elam Barker <beb82@cornell.edu>
2019-03-07 22:07:44 +0000

    changed runSteadyCom to default to individual small (currently fixed) growth rate lower bound per species)

--- Utils/runSteadyCom.m
+++ Utils/runSteadyCom.m
@@ -6,15 +6,41 @@ function [sol, result] = runSteadyCom(multiModel)
   options=struct();
   options.algorithm = 3;
 
-  % We're assuming proportial in this case (make a flag later)
-  %
-  % nSpecies = length(multiModel.infoCom.spAbbr);
-  % options.BMrhs = [nSpecies];
-  % options.BMcsense = 'E';
-  % bmCon(1:nSpecies) = 1;
-  % options.BMcon = bmCon;
+% We're assuming proportial in this case (make a flag later)
+%
+nSpecies = length(multiModel.infoCom.spAbbr);
+% options.BMrhs = [nSpecies];
+% options.BMcsense = 'E';
+% bmCon(1:nSpecies) = 1;
+% options.BMcon = bmCon;
 
-  origFeasTol = getCobraSolverParams('LP', 'feasTol');
-  changeCobraSolverParams('LP', 'feasTol', 1e-8);
-  [sol result] = SteadyCom(multiModel, options);
-  changeCobraSolverParams('LP', 'feasTol', origFeasTol);
+%
+%  Somewhat confusingly, these are not fixing the individual X_i to be proportial
+%  (althouch could be used to do so); BMcon = [1 1 1 ... 1], BMrhs = [1];
+%  corresponds to the constraint in the paper Sum(Vbm_k) = u * Sum(X_k)
+%
+% * BMcon - Biomass constraint matrix :math:`(\sum (a_{ij} * X_j) </=/> b_i)`
+%  (given as :math:`K * N_{organisms}` matrix for `K` constraints)
+%   e.g. [0 1 1 0] for :math:`X_2 + X_3` in a 4-organisms model
+%  * BMrhs - RHS for BMcon, `K x 1` vector for `K` constraints
+%
+% Requiring them all to sum to one, however, seems to be not restrictive enough:
+% most species still won't grow. On the other hand, having this constraint with
+% individual species constraints is apparently too restrictive to get biologically
+% meaningful results (why?)
+%
+% options.BMcon = ones(1, nSpecies);
+% options.BMrhs = [1];
+% options.BMcsense = ['E'];
+%
+% So instead, we opt for setting individual small constraints,
+% which seems to work well:
+options.BMcon = diag(ones(nSpecies, 1));
+options.BMrhs = repmat(0.01, nSpecies, 1)'
+options.BMcsense = [strjoin(repmat({'G'}, nSpecies, 1), '')];
+
+
+origFeasTol = getCobraSolverParams('LP', 'feasTol');
+changeCobraSolverParams('LP', 'feasTol', 1e-8);
+[sol result] = SteadyCom(multiModel, options);
+changeCobraSolverParams('LP', 'feasTol', origFeasTol);
