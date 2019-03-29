439ad811e157d4f430890ce82f02e41219ff5e8b
Brandon Elam Barker <beb82@cornell.edu>
2019-03-29 00:57:07 +0000

    adding a non-memoized version of cellPowerSetFilter

---
+++ Utils/cellPowerSetFilter.m
@@ -0,0 +1,37 @@
+%
+% This is a recursive function, so it will not work on large sets.
+%
+% fun is the operation applied to each subset, parent value pair.
+%     It is NOT applied to the empty set
+%
+% pred is applied to the output of fun;
+% if true, then the result is eventually returned, and the function
+% recurses.
+%
+% comp: it is up to the function implementation to record which sets
+%       are being compared in the output
+%
+% Example: To just show subsets with parents in comps:
+% cellPowerSetFilter(@(x) x, @(x) true, someSet, @(v,pv) {pv,v})
+%
+function [ssVals, comps] = cellPowerSetFilter(fun, pred, carray, compare)
+  setInit = {carray{:}};
+  ssVals = {};
+  comps = {};
+  valInit = fun(setInit);
+  loop(setInit, valInit);
+
+  function loop(ssetIn, parentValue)
+    ssVals{end+1} = fun(ssetIn);
+    comps{end+1} = compare(ssVals{end}, parentValue);
+    nSubs = numel(ssetIn);
+    if pred(ssVals{end}) && nSubs > 1
+      for ii = 1:nSubs
+        sset = ssetIn;
+        sset(ii) = [];
+        loop(sset, ssetIn);
+      end
+    end
+  end
+
+end
---
+++ Utils/identity.m
@@ -0,0 +1,2 @@
+function out = identity(val)
+  out = val
