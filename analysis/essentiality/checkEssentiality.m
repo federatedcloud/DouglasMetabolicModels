cb0fcb496b5e02c6487e4724dbb1e4cdeb725951
Brandon Elam Barker <beb82@cornell.edu>
2019-04-11 17:28:28 +0000

    added simple script for conducting essentiality analysis using FBA and SteadyCom

---
+++ analysis/essentiality/checkEssentiality.m
@@ -0,0 +1,17 @@
+function essentialRxns = checkEssentiality(model, rxns)
+  essentialRxns = {};
+  numRxns = numel(rxns);
+  for ii = 1:numRxns
+    modelDel = changeRxnBounds(model, rxns(ii), 0, 'b');
+    [sol, res] = runSteadyCom(modelDel);
+    fbaRes = optimizeCbModel(modelDel);
+    if fbaRes.f < 1e-7 || sum(res.vBM) < 1e-6
+      output = struct;
+      output.rxn = rxns(ii);
+      output.fbaGrowth = fbaRes.f;
+      output.scomGrowth = res.GRmax;
+      output.vBM = res.vBM;
+      essentialRxns{end+1} = output;
+    end
+  end
+end
