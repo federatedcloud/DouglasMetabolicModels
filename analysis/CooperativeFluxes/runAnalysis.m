39b97ae1ec1a93ac4d0c8e64e013233ca6a6c5db
Brandon Elam Barker <beb82@cornell.edu>
2019-05-11 19:11:08 +0000

    adding CMP analysis, but need to add child to map for individual analysis

--- analysis/CooperativeFluxes/runAnalysis.m
+++ analysis/CooperativeFluxes/runAnalysis.m
@@ -28,4 +28,52 @@ function analysis = runAnalysis(modelMap, mediaType)
     fName = strjoin({outDirectory, filesep, comp.label, '.csv'}, '');
     writetable(comp.table, fName);
   end
+  % Summary Comparison analysis
+  dfName = strjoin({outDirectory, filesep, 'differential', '.csv'}, '');
+  dHeader = strjoin({ ...
+    'Communities', 'Child Size', 'Parent Size', ...
+    'Child_Lost_rxns', 'Parent_Gained_rxns', ...
+    'CMP_Diff', 'CMP_EX_Diff', ...
+    'CMP_Child', 'CMP_Parent', ...
+    'CMP_EX_Child', 'CMP_EX_Parent'}, ',');
+  fid = fopen(dfName, 'wt+');
+  fprintf(fid, '%s\n', dHeader);
+  for ii = 1:numel(comparisons)
+    comp = comparisons{ii};
+    cmpDiff = comp.cmp.parent - comp.cmp.child;
+    cmpExDiff = comp.cmpEx.parent - comp.cmpEx.child;
+    childLost = strjoin(comp.childLostRxns, ';');
+    parGained = strjoin(comp.parentGainedRxns, ';');
+    fprintf(fid, '%s,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d\n', ...
+      comp.label, comp.size.child, comp.size.parent, ...
+      childLost, parGained, ...
+      cmpDiff, cmpExDiff, ...
+      comp.cmp.child, comp.cmp.parent, ...
+      comp.cmpEx.child, comp.cmpEx.parent);
+  end
+  fclose(fid);
+
+  % Individual community analysis
+  cmpMap = containers.Map();
+  for ii = 1:numel(comparisons)
+    comp = comparisons{ii};
+    cmpData = struct;
+    % Just use the parent case:
+    cmpData.label = comp.parName;
+    cmpData.size = comp.size.parent;
+    cmpData.cmp = comp.cmp.parent;
+    cmpData.cmpEx = comp.cmpEx.parent;
+    cmpMap(cmpData.label) = cmpData;
+  end
+  sfName = strjoin({outDirectory, filesep, 'individual', '.csv'}, '');
+  sHeader = strjoin({'Community', '#species', 'CMP', 'CMP_EX'}, ',');
+  fid = fopen(sfName, 'wt+');
+  fprintf(fid, '%s\n', sHeader);
+  commLabels = keys(cmpMap);
+  for ii = 1:numel(commLabels)
+    label = commLabels{ii};
+    rec = cmpMap(label);
+    fprintf(fid, '%s,%d,%d,%d\n', rec.label, rec.size, rec.cmp, rec.cmpEx);
+  end
+  fclose(fid);
 end
