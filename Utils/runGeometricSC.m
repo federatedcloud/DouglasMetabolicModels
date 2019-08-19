4a96a32f479bd372479e58914c903c694e8f661a
Brandon Elam Barker <beb82@cornell.edu>
2019-08-19 19:53:47 +0000

    checking in current best params for geometricSC

--- Utils/runGeometricSC.m
+++ Utils/runGeometricSC.m
@@ -3,5 +3,5 @@ function flux = runGeometricSC(multiModel)
   options = steadyComDefs(multiModel);
 
   origFeasTol = getCobraSolverParams('LP', 'feasTol');
-  flux = geometricSC(multiModel, options, 'flexRel', 1e-3, 'epsilon', 1e-9);
+  flux = geometricSC(multiModel, options, 'flexRel', 1e-3, 'epsilon', 1e-6);
 end
