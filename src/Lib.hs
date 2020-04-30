6d0312bc6d80a24ae2aa93ded802f35f490eb1ef
Brandon Elam Barker <beb82@cornell.edu>
2020-04-30 21:44:37 +0000

    prototyped array creation function

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,6 +2,8 @@
 
 module Lib where
 
+import Data.Foldable (toList)
+import Data.List (permutations)
 import Foreign.Matlab
 import Foreign.Matlab.Array
 import Foreign.Matlab.Engine
@@ -12,3 +14,21 @@ import Path
 type MArrayFun a = forall b. MType b b => a -> IO (MXArray b)
 
 
+-- | Note that if `f` is IO, the effect is not run,
+-- | as it is not necessary in a lazy language.
+absolveUnits :: f () -> ()
+absolveUnits _ = ()
+
+
+runAll :: Monad t => [t ()] -> t ()
+runAll as = absolveUnits <$> sequence as
+
+permList :: Integer -> [[Integer]]
+permList n = permutations [1..n]
+
+
+travToMXArray :: (Traversable t, MXArrayComponent a) => t a -> MIO (MXArray a)
+travToMXArray xs = do
+  mxarr <- createMXArray [1, length xs]
+  mxArraySetAll mxarr (toList xs) 
+  pure mxarr
