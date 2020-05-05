05926abfc06901e223cadf3c173e0de6538cc546
Brandon Elam Barker <beb82@cornell.edu>
2020-05-05 21:27:42 +0000

    drafting priority-effects constraint function

--- Utils/semiDynamicSteadyCom.m
+++ Utils/semiDynamicSteadyCom.m
@@ -10,26 +10,53 @@ function [sol, result, LP, LPminNorm, indLP] = semiDynamicSteadyCom(modelMap, sc
 
 % TODO: at what level do we handle Memoization, e.g. https://wiki.haskell.org/Memoization
 
-numSteps = length(schedule);
-
-initCom = modelMap(schedule(1));
-
-currentOrgKeys = {};
-currentBounds = [];
-
-for ii = 1:numSteps
-  % Currently, a schedule has a single organism at each step:
-  currentOrgKeys = union(currentOrgKeys, {schedule(ii)});
-
-  modelCom = makeMultiModel(currentOrgKeys, modelMap, 'unbounded');
-
-  %TODO: need to adjust this to work for exchange-reactions only, etc.
-  modelCom = updateBounds(modelCom, rxns, lbs);
-  currentBounds = modelMap(schedule(ii)); % TODO: probalby need an aux function for this for ii /= 1.
+  fluxThresh = 1e-7;
+  growThresh = 1e-7;
   
+  numSteps = length(schedule);
 
-  [sol result LP LPminNorm] = SteadyCom(modelCom, schedule, options, varargin);
+  initCom = modelMap(schedule(1));
+
+  currentOrgKeys = {};
+  fluxPrior = [];
+
+  modelLast = {};
+
+  for ii = 1:numSteps
+    % Currently, a schedule has a single organism at each step:
+    currentOrgKeys = union(currentOrgKeys, {schedule(ii)});
+
+    modelCom = makeMultiModel(currentOrgKeys, modelMap, 'minimal-plus');
+    essentialRxns = checkEssentiality(modelCom, rxns);
+
+    if ii > 1
+      modelCom.lb = updateLB(modelCom, modelPrior, essInfo)
+    end
+
+    [sol result LP LPminNorm] = SteadyCom(modelCom, schedule, options, varargin);
+    fluxPrior = result.flux;
+    result.essential = essentialRxns;
+  end
+
+
+  # TODO: priorFlux should probably be calculated as the minFVA value
+  function newLB = updateLB(model, modelPrior, essInfo)
+    newLB = model.lb;
+
+    for ri == 1:length(essInfo)
+      essi = essInfo{ii};
+      % If it is not essential:
+      if (essi.scomGrowth > growThresh)
+	rxn = essi.rxn;
+	rxnIxPrior = find(strcmp(modelPrior.rxns, rxn));
+	rxnIxCurr = find(strcmp(model.rxns, rxn));
+
+        % It is being consumed priorly, so we constrain to zero
+	if fluxPrior(rxnIxPrior) < -fluxThresh
+          newLB(rxnIxCurr) = 0;
+        end
+      end
+    end
+  end
   
 end
-							 
-  
